01626754a672a3f9b3cf5cf29adbba82aa9dfe24
shafalam <38904470+shafalam@users.noreply.github.com>
2019-12-23 12:22:18 +0200

    Updates

---
+++ precache-manifest.69e5b0384ec70cde256c22fbf8547d6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614d037c0482aae7cb853c63cd6004c0",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "ae5dba9e42e49f031570",
+    "url": "/tic-tac-toe/static/css/main.be25fa0a.chunk.css"
+  },
+  {
+    "revision": "352f3aaeed4ace6b2d8b",
+    "url": "/tic-tac-toe/static/js/2.e851ae0b.chunk.js"
+  },
+  {
+    "revision": "ae5dba9e42e49f031570",
+    "url": "/tic-tac-toe/static/js/main.76db7f38.chunk.js"
+  },
+  {
+    "revision": "25d94bd95813189daa86",
+    "url": "/tic-tac-toe/static/js/runtime-main.00b9c82d.js"
+  }
+]);
